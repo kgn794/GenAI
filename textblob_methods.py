f80578f85decf092c4d6b56015607864c6075080
kgn794 <170333726+kgn794@users.noreply.github.com>
2026-07-01 08:59:55 +0530

    Update textblob_methods.py

--- textblob_methods.py
+++ textblob_methods.py
@@ -4,7 +4,17 @@ from textblob import TextBlob
 from collections import Counter
 import re
 from textblob import Word
+import ssl
+import nltk
 
+ssl._create_default_https_context = ssl._create_unverified_context
+
+nltk.download('brown')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('conll2000')
+nltk.download('movie_reviews')
 
 st.subheader("Sentiment Analysis")
 str = "Python is amazing!"
