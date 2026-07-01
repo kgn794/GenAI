08c1d919c120cb3517e274383f796b0992648328
kgn794 <170333726+kgn794@users.noreply.github.com>
2026-07-01 07:24:47 +0530

    Update UI_methods.py

--- UI_methods.py
+++ UI_methods.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 import time
 
+
 st.title("Title - title")
+st.divider()
+st.empty()
 st.header("Header - header")
 st.subheader("Sub Header - subheader")
 st.text("Plain text")
